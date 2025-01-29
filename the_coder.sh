571900274e5f998402a95c00552d21edfbe97ba0
Sarah Gasaro <s.gasaro1@alustudent.com>
2025-01-29 02:30:48 -0800

    modified the coder too

--- the_coder.sh
+++ the_coder.sh
@@ -1,3 +1,12 @@
-echo"Angel kibui "
-echo "Sarah Gasaro"
-echo"Darcy Mbanza"
+echo"Angel kibui" >> cohort-1.txt
+echo"Sarah Gasaro" >> cohort-1.txt
+echo"Darcy Mbanza" >>  cohort-1.txt
+
+echo"Divine Ikirezi" >> cohort-2.txt
+echo"Vincent Manzi" >> cohort-2.txt
+echo"Kevine Uwisanga" >> cohort-2.txt
+
+echo"Tesy Shimwa" >> cohort-3.txt
+echo"Cassie Kivuye" >> cohort-3.txt
+echo"Seth Iradu" >> cohort-3.txt
+
